c23616be2711e531cf2e07bd7a9ef38cb9d14490
piyushpanjwani12 <piyushpanjwani12@gmail.com>
2019-07-09 01:02:54 +0530

    2 files modified

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, HostListener } from '@angular/core';
+import {SidebarComponent} from "../sidebar/sidebar.component";
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  @Input() sideBarElement: SidebarComponent;
 
+  @HostListener('click')
+  click() {
+    this.sideBar.toggle();
+  }
   constructor() { }
 
   ngOnInit() {
