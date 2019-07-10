9e5c8eaab23624172dbd6d7b18bc1c62dbe5fcc3
piyushpanjwani12 <piyushpanjwani12@gmail.com>
2019-07-10 10:22:42 +0530

    10 files modified, 1 added

---
+++ src/scripts.js
@@ -0,0 +1,14 @@
+Console.log ( "trial");
+      function allowDrop(ev) { 
+                ev.preventDefault(); 
+      } 
+              
+      function dragStart(ev) { 
+           ev.dataTransfer.setData("text", ev.target.id); 
+      } 
+              
+      function dragDrop(ev) { 
+                ev.preventDefault(); 
+                var data = ev.dataTransfer.getData("text"); 
+                ev.target.appendChild(document.getElementById(data)); 
+      } 
