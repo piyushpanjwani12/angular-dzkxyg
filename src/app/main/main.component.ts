2b73e00988d471cfbd5ccb982d50b9ecc9301e37
piyushpanjwani12 <piyushpanjwani12@gmail.com>
2019-07-14 02:08:53 +0530

    8 files modified, 1 added

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,14 +7,22 @@ import {SidebarComponent} from "../sidebar/sidebar.component";
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-  
+  isElement1Dropped: boolean;
+  isElement2Dropped: boolean;
   drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    if(ev.target.id!=="drag1" && ev.target.id!=="drag2"){
+      ev.target.appendChild(document.getElementById(data));
+    }
+    
   }
 
   allowDrop(ev) {
+    // if(ev.target.id==="dropHere")
+    // document.getElementById("myCard").style.display="none";
+    // if(ev.target.id==="dropHere1")
+    // document.getElementById("myCard1").style.display="none";
     ev.preventDefault();
   }
 
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -9,27 +9,38 @@ import { HttpClient } from '@angular/common/http';
 })
 export class UsersComponent implements OnInit {
   httpdata;
+  pages:number[]=[
+    1,2,3,4,5,6,7,8,9,10
+  ];
   config: any;
   collection = { count: 60, data: [] };
   displaydata(data) {this.httpdata = data;}
   constructor(private http: HttpClient) { 
     
  
-    this.config = {
-      itemsPerPage: 5,
-      currentPage: 1,
-      totalItems: 10,
-    };
+    
   }
- 
-  pageChanged(event){
-    this.config.currentPage = event;
-  }
-  
-  
+
   ngOnInit() {
-    this.http.get("https://jsonplaceholder.typicode.com/users").
+    this.http.get("https://5d283619bc7faf001460f3dd.mockapi.io/models/dummy1/users/?page=1&limit=8").
       subscribe((data) => {this.displaydata(data);})
   }
-  
+  public updatePage(page){
+    this.http.get("https://5d283619bc7faf001460f3dd.mockapi.io/models/dummy1/users/?page="+page+"&limit=8").
+      subscribe((data) => {this.displaydata(data);})
+  }
+  public nextPage(page){
+    var pageNo= parseInt(page, 10);
+     console.log(pageNo);
+    pageNo=pageNo+1;
+   
+    this.http.get("https://5d283619bc7faf001460f3dd.mockapi.io/models/dummy1/users/?page="+page+"&limit=8").
+      subscribe((data) => {this.displaydata(data);})
+  }
+  public previousPage(page){
+    if(page!=1){
+      this.http.get("https://5d283619bc7faf001460f3dd.mockapi.io/models/dummy1/users/?page="+(page-1)+"&limit=8").
+      subscribe((data) => {this.displaydata(data);})
+    }
+  }
 }
